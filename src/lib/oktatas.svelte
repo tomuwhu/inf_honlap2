10b0a05389d52f92cb7799b31d5bce0850fb23cd
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-11 10:53:40 +0100

    .

--- src/lib/oktatas.svelte
+++ src/lib/oktatas.svelte
@@ -13,6 +13,7 @@
         {name: 'Informatikai alkalmazások', years: ['2016-2022'], mds: ['infalk']},
         {name: 'Oktatást támogató informatikai rendszerek', years: ['2016-2024'], mds: ['otir']},
         {name: 'Programozási versenyek feladatainak megoldása', years: ['2016-2024'], mds: ['pmsz']},
+        {name: 'Webes technológiák - SvelteKit/Drizzle/Turso/Lucia', years: ['tervezett: 2025 ősz'], mds: ['wt']},
     ])
     var ssyears = $derived(list[selected_subject - 1].years)
     let current_url = '/inf_honlap2'
